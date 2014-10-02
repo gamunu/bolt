c1a2956f02656f3ec2c244486a816337126905ae
Gamunu Bandara <Gamunuud@gmail.com>
2014-10-02 13:40:03 +0530

    Unix build starts, repositary is unstable now

---
+++ casablanca/build/CMakeFiles/casablanca.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gamunu/bolt/casablanca/src/http/client/http_linux.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/http/client/http_linux.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/http/client/http_msg_client.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/http/client/http_msg_client.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/http/client/x509_cert_utilities.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/http/client/x509_cert_utilities.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/http/common/http_helpers.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/http/common/http_helpers.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/http/common/http_msg.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/http/common/http_msg.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/http/listener/http_linux_server.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/http/listener/http_linux_server.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/http/listener/http_listener.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/http/listener/http_listener.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/http/listener/http_msg_listen.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/http/listener/http_msg_listen.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/http/listener/http_server_api.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/http/listener/http_server_api.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/http/oauth/oauth1.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/http/oauth/oauth1.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/http/oauth/oauth2.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/http/oauth/oauth2.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/json/json.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/json/json.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/json/json_parsing.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/json/json_parsing.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/json/json_serialization.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/json/json_serialization.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/pplx/linux/pplxlinux.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/pplx/linux/pplxlinux.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/pplx/pplx.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/pplx/pplx.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/pplx/threadpool.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/pplx/threadpool.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/streams/linux/fileio_linux.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/streams/linux/fileio_linux.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/uri/uri.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/uri/uri.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/uri/uri_builder.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/uri/uri_builder.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/uri/uri_parser.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/uri/uri_parser.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/utilities/asyncrt_utils.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/utilities/asyncrt_utils.cpp.o"
+  "/home/gamunu/bolt/casablanca/src/utilities/base64.cpp" "/home/gamunu/bolt/casablanca/build/CMakeFiles/casablanca.dir/src/utilities/base64.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../src/pch"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
